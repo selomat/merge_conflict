a39a62ab950d9df55d85df0b13872cd4a60127d8
selim.kaya <kayaselim303@gmail.com>
2020-12-06 13:15:48 +0100

    main branchde ilk 3satir degidti

--- coflict.js
+++ coflict.js
@@ -1,1 +1,3 @@
-// bu aciklama main branchde gerceklesti
+// Bu aciklama main branchde eklendi. mainde iken tekrar degisti
+let main = "main branchde eklendi";
+let farkliSatir ="mainde farkli satira bir degisken daha eklendi";  
